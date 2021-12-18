cacf1e86deb2fad29e98ab8cfd67e784c59d3c1f
abdulrahmanalaa <abdulrahmanalaa@hotmail.com>
2021-12-18 15:21:23 +0400

    post listing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import PostCreate from "./Modules/Posts/PostCreate";
+import PostList from "./Modules/Posts/PostList";
 export const App = () => (
   <div className="container">
     <h1>Create Post</h1>
     <PostCreate></PostCreate>
+    <hr />
+    <h2>Posts</h2>
+    <PostList></PostList>
   </div>
 );
