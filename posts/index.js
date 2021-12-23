fb4f4d2b06f12dba841072c8df9d51b172506971
abdulrahmanalaa <abdulrahmanalaa@hotmail.com>
2021-12-24 01:55:41 +0400

    adding first event in post created

--- posts/index.js
+++ posts/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,10 +12,14 @@ app.get("/posts", (req, res) => {
   res.status(200).send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { title, id };
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
   res.status(201).send(posts[id]);
 });
 
